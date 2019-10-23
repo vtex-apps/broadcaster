95f57132f0e126fa48cc4c837c30c80c490b80be
NataliaGodot <natalia_godot@hotmail.com>
2019-10-23 15:45:10 -0300

    Use 10% of traffic

--- node/middlewares/settings.ts
+++ node/middlewares/settings.ts
@@ -23,7 +23,7 @@ export async function settings(ctx: Context, next: () => Promise<any>) {
   // to go forward, you can replace the following line with
   // the following code
   //    const enabledGlobally = Math.random() < 0.7
-  const enabledGlobally = Math.random() < 0.01
+  const enabledGlobally = Math.random() < 0.1
 
   const { enabled: enabledInWorkspace } = enabledGlobally 
     ? await apps.getAppSettings(VTEX_APP_ID).then(parseSettings)
