fc5feb3090f61ddfec0461f6d8286e8a5f91e9a2
Victor Raposo <vwraposo@gmail.com>
2020-02-11 11:37:13 -0300

    Adds throttling middleware

---
+++ node/middlewares/throttle.ts
@@ -0,0 +1,19 @@
+import { TooManyRequestsError } from '@vtex/api'
+
+const MAX_REQUEST = 50 
+let COUNTER = 0
+
+export async function throttle(
+  _: Context,
+  next: () => Promise<void>
+) {
+  COUNTER++
+  try {
+    if (COUNTER > MAX_REQUEST) {
+      throw new TooManyRequestsError()
+    }
+    await next()
+  } finally {
+    COUNTER--
+  }
+}
