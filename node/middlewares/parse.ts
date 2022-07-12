dd81980fc6a2a397dfab15b76cc250c9d2691241
Alberto Mendoza <albertm_805@outlook.com>
2022-07-12 01:08:24 -0500

    [NOTIFIER-42] remove motorola from blacklist

--- node/middlewares/parse.ts
+++ node/middlewares/parse.ts
@@ -3,8 +3,7 @@ import { json } from 'co-body'
 import { ENABLED_GLOBALLY } from '../constants'
 
 export async function parseAndValidate(ctx: Context, next: () => Promise<any>) {
-  const { account } = ctx.vtex
-  if (!ENABLED_GLOBALLY || account.startsWith('motorola')) {
+  if (!ENABLED_GLOBALLY) {
     ctx.body = 'Service not enabled.'
     ctx.status = 200
     return
